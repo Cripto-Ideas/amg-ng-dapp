98d08100af86a498f55d5ea3e224a6c922f94ae0
Tecla Roig <ch.roig@gmail.com>
2022-01-25 19:48:08 -0300

    polyfills error

--- src/app/services/blockchain.service.ts
+++ src/app/services/blockchain.service.ts
@@ -1,31 +1,48 @@
 import { Injectable } from '@angular/core';
+import Web3 from 'web3';
 
 @Injectable({
   providedIn: 'root'
 })
 export class BlockchainService {
 
-  constructor() { }
+  private windowBrowser: any;
+  private mainAccount: any;
+
+  constructor() { 
+    this.windowBrowser = window;
+  }
 
   async loadWeb3() {
     
-    console.log((window as any).ethereum);
+    console.log(this.windowBrowser.ethereum);
 
-    /*if((window as any).ethereum) {
+    if(this.windowBrowser.ethereum) {
       console.log('es eth');
-      (window as any).web3 = new Web3((window as any).ethereum)
-      await (window as any).ethereum.enable()
+      this.windowBrowser.web3 = new Web3(this.windowBrowser.ethereum)
+      await this.windowBrowser.ethereum.enable()
     }
 
-    else if ((window as any).web3) {
-      (window as any).web3 = new Web3((window as any).web3.currentProvider)
+    else if (this.windowBrowser.web3) {
+      this.windowBrowser.web3 = new Web3(this.windowBrowser.web3.currentProvider)
     }
 
     else {
       window.alert('¡Considera usar Metamask!')
     }
 
-    console.log((window as any).web3);*/
+    console.log(this.windowBrowser.web3);
   }
+
+/*  async loadBlockchainData() {
+    const web3 = this.windowBrowser.web3
+    // Cargar una cuenta
+    const accounts = await web3.eth.getAccounts()
+    console.log(accounts);
+    console.log(await web3.eth);
+    
+    this.mainAccount = accounts[0];   
+    console.log(this.mainAccount);
+  }*/
 
 }
